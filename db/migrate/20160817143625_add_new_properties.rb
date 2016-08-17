41d96098e460d6a9512835050890fe34001a2220
m <m@ophect.me>
2016-08-17 11:21:31 -0400

    fixes bungled migration; resets the line height in li and p elements

--- db/migrate/20160817143625_add_new_properties.rb
+++ db/migrate/20160817143625_add_new_properties.rb
@@ -1,18 +1,19 @@
 class AddNewProperties < ActiveRecord::Migration
-  PROPERTIES = {
-    'HeaderLeft' => "#{prop('HeaderName')}\r\n#{prop('HeaderSubtitle')}",
-    'HeaderRight' => "#{prop('HeaderEmail')}\r\n#{prop('HeaderGitHubAccountName')}"
-  }
-
   def up
-    PROPERTIES.each { |k, v| Property.create(:key => k, :value => v) }
+    properties.each { |k, v| Property.create(:key => k, :value => v) }
   end
 
   def down
-    PROPERTIES.each { |k, _| Property.find_by_key(k).try(:delete) }
+    properties.each { |k, _| Property.find_by_key(k).try(:delete) }
   end
 
   private
+
+  def properties
+    { 'HeaderLeft' => "#{prop('HeaderName')}\r\n#{prop('HeaderSubtitle')}",
+      'HeaderRight' => "#{prop('HeaderEmail')}\r\n#{prop('HeaderGitHubAccountName')}"
+    }
+  end
 
   def prop(k)
     Property.find_by_key(k).value
